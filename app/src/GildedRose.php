d1ba7ed57219fd002e2ea7d96ea20896a74dd36c
ganiulis <gintarasa95@gmail.com>
2021-10-26 22:40:39 +0300

    added Conjured items functionality

--- app/src/GildedRose.php
+++ app/src/GildedRose.php
@@ -57,11 +57,27 @@ final class GildedRose
                     $item->sell_in -= 1;
                     break;
 
+                case 'Conjured Mana Cake':
+                    if ($item->sell_in < 1 && $item->quality > 3)
+                    {
+                        $item->quality -= 4;
+                    }
+                    elseif ($item->quality > 1)
+                    {
+                        $item->quality -= 2;
+                    }
+                    elseif ($item->quality > 0)
+                    {
+                        $item->quality -= 1;
+                    }
+                    $item->sell_in -= 1;
+                    break;
+
                 case 'Sulfuras, Hand of Ragnaros':
                     break;
 
                 default:
-                    if ($item->quality > 1 && $item->sell_in < 1)
+                    if ($item->sell_in < 1 && $item->quality > 1)
                     {
                         $item->quality -= 2;
                     }
