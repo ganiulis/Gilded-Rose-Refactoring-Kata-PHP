f36d498f528aac6c500376483b40cf6a075850da
ganiulis <gintarasa95@gmail.com>
2021-10-26 22:04:12 +0300

    slight refactoring with switch statements

--- app/src/GildedRose.php
+++ app/src/GildedRose.php
@@ -18,51 +18,56 @@ final class GildedRose
 
     public function updateQuality(): void
     {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+        foreach ($this->items as $item) 
+        {
+            switch($item->name)
+            {
+                case 'Aged Brie':
+                    if ($item->sell_in < 1 && $item->quality < 49)
+                    {
+                        $item->quality += 2;
                     }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    elseif ($item->quality < 50)
+                    {
+                        $item->quality += 1;
+                    }
+                    $item->sell_in -= 1;
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    if ($item->sell_in < 1)
+                    {
+                        $item->quality = 0;
+                    }
+                    else
+                    {
+                        if ($item->sell_in < 6 && $item->quality < 48)
+                        {
+                            $item->quality += 3;
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                        elseif ($item->sell_in < 11 && $item->quality < 49)
+                        {
+                            $item->quality += 2;
+                        }
+                        elseif ($item->quality < 50)
+                        {
+                            $item->quality += 1;
                         }
                     }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
+                    $item->sell_in -= 1;
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    break;
+                default:
+                    if ($item->quality > 1 && $item->sell_in < 1)
+                    {
+                        $item->quality -= 2;
                     }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    elseif ($item->quality > 0)
+                    {
+                        $item->quality -= 1;
                     }
-                }
+                    $item->sell_in -= 1;
+                    break;
             }
         }
     }
