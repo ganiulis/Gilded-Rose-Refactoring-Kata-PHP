e1b4a2d866941919d87365c199b9610c464f9f43
ganiulis <gintarasa95@gmail.com>
2021-10-28 20:38:12 +0300

    changed ApprovalTest.php to include imploded newlines

--- app/tests/ApprovalTest.php
+++ app/tests/ApprovalTest.php
@@ -12,7 +12,9 @@ class ApprovalTest extends TestCase
     public function testTestFixture(): void
     {
         exec('php fixtures/texttest_fixture.php 31', $output);
-        
-        Approvals::verifyString(implode(PHP_EOL, $output));
+
+        $implodedFixture = implode("\n", $output);
+
+        Approvals::verifyString($implodedFixture);
     }
 }
