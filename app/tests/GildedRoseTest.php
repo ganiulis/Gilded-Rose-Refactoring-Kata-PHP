eceb7fd4178dba2dd6aee26ecf53b81e0a47cd9a
ganiulis <gintarasa95@gmail.com>
2021-10-28 20:17:56 +0300

    refactored test data for readability and added more tests

--- app/tests/GildedRoseTest.php
+++ app/tests/GildedRoseTest.php
@@ -13,63 +13,80 @@ class GildedRoseTest extends TestCase
     /**
      * @dataProvider provideItemData
      */
-    public function testItem($name, $sellIn, $quality): void
-    {
-        $itemTest = new Item($name, $sellIn['SellIn test'], $quality['Quality test']);
-        $itemResult = new Item($name, $sellIn['SellIn result'], $quality['Quality result']);
+    public function testItem(array $testData): void {
+        $itemTest = new Item($testData['Name'], $testData['SellIn']['test'], $testData['Quality']['test']);
+        $itemExpected = new Item($testData['Name'], $testData['SellIn']['expected'], $testData['Quality']['expected']);
 
         $gildedRose = new GildedRose([$itemTest]);
         
         $gildedRose->updateQuality();
 
-        $this->assertEquals($itemResult, $itemTest);
+        $this->assertEquals($itemExpected, $itemTest);
     }
 
-    public function provideItemData()
-    {
+    public function provideItemData(): array {
         return [
             [
-                'Mana Cake',
-                [
-                    'SellIn test' => 6, 
-                    'SellIn result' => 5
-                ],
-                [
-                    'Quality test' => 2, 
-                    'Quality result' => 1
+                'Typical item - SellIn and Quality both drop by 1' => [
+                    'Name' => 'Mana Cake',        
+                    'SellIn' => ['test' => 5, 'expected' => 4],
+                    'Quality' => ['test' => 5, 'expected' => 4]
                 ]
             ],
             [
-                'Conjured Cake',
-                [
-                    'SellIn test' => 6, 
-                    'SellIn result' => 5
-                ],
-                [
-                    'Quality test' => 2, 
-                    'Quality result' => 0
+                'Conjured item - Quality drops by 2 when SellIn drops by 1' => [
+                    'Name' => 'Conjured Cake',    
+                    'SellIn' => ['test' => 5, 'expected' => 4],
+                    'Quality' => ['test' => 5, 'expected' => 3]
                 ]
             ],
             [
-                'nonConjured Cake',
-                [
-                    'SellIn test' => 6, 
-                    'SellIn result' => 5
-                ],
-                [
-                    'Quality test' => 2, 
-                    'Quality result' => 1
+                'Regex test for Nonconjured item - SellIn and Quality both drop by 1' => [
+                    'Name' => 'Nonconjured Cake', 
+                    'SellIn' => ['test' => 5, 'expected' => 4],
+                    'Quality' => ['test' => 5, 'expected' => 4]
                 ]
             ],
             [
-                'Water',
-                [
-                    'SellIn test' => 0, 
-                    'SellIn result' => -1
-                ],
-                [
-                    'Quality test' => 2, 
-                    'Quality result' => 0
+                'Typical item - drops in Quality by 2 when updated SellIn becomes negative' => [
+                    'Name' => 'Water',
+                    'SellIn' => ['test' => 0, 'expected' => -1],
+                    'Quality' => ['test' => 5, 'expected' => 3]
+                ]
+            ],
+            [
+                'Typical item - drops in Quality by 2 when updated SellIn is already negative' => [
+                    'Name' => 'Water',
+                    'SellIn' => ['test' => -1, 'expected' => -2],
+                    'Quality' => ['test' => 5, 'expected' => 3]
+                ]
+            ],
+            [
+                'Sulfuras - does not change' => [
+                    'Name' => 'Sulfuras, Hand of Ragnaros',    
+                    'SellIn' => ['test' => 0, 'expected' => 0],
+                    'Quality' => ['test' => 80, 'expected' => 80]
+                ]
+            ],
+            [
+                'Conjured item - drops in Quality by 4 when updated SellIn becomes negative' => [
+                    'Name' => 'Conjured Cake',    
+                    'SellIn' => ['test' => 0, 'expected' => -1],
+                    'Quality' => ['test' => 5, 'expected' => 1]
+                ]
+            ],
+            [
+                'Conjured item - drops in Quality by 4 when updated SellIn is already negative' => [
+                    'Name' => 'Conjured Cake',    
+                    'SellIn' => ['test' => -1, 'expected' => -2],
+                    'Quality' => ['test' => 5, 'expected' => 1]
+                ]
+            ],
+            [
+                'Conjured item - drops in Quality to zero' => [
+                    'Name' => 'Conjured Cake',    
+                    'SellIn' => ['test' => -1, 'expected' => -2],
+                    'Quality' => ['test' => 3, 'expected' => 0]
                 ]
             ]
         ];
